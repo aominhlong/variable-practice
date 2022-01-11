e6a596433cd07e3dfa27aa71083ac7fdfb350d71
Nicholas Ao <aominhlong@yahoo.com>
2022-01-10 20:20:53 -0700

    add waterBoy

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 var name = "Nick"
-var name = "Mama says that alligators are angry cause they got all dem teeth, but no toothbrush"
+var waterBoy = "Mama says that alligators are angry cause they got all dem teeth, but no toothbrush"
 var luckyNumber = 16
 var unluckyNumber = 13
 var isTall = false
