e94a33de91f3f61cbbffa9838232ccbccd3cb023
Nicholas Ao <aominhlong@yahoo.com>
2022-01-10 20:20:06 -0700

    add more variables

--- variables.js
+++ variables.js
@@ -1,3 +1,6 @@
 var name = "Nick"
+var name = "Mama says that alligators are angry cause they got all dem teeth, but no toothbrush"
 var luckyNumber = 16
+var unluckyNumber = 13
 var isTall = false
+var isSmall = false
